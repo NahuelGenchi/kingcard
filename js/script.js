34a6a9a8f4fa013188e83fa024391198cca4e3da
Nahuel Genchi <nagen01@gmail.com>
2022-05-21 23:13:23 -0300

    Fixing texts

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 let ageInput = document.querySelector("#ageInput");
 let ageBtn = document.querySelector("#ageBtn");
+let ageText = document.querySelector("#ageText");
 let elegibleText = document.querySelector("#elegible");
 let age;
 
 ageInput.focus();
 ageBtn.addEventListener("click", function(){
     age = ageInput.value;
+    ageText.textContent = `Your age is ${age}`;
     if (age < 100) {
-        elegibleText.textContent = `Your age is ${age}. You are not elegible.`;
+        elegibleText.textContent = `You are not elegible.`;
     } else if (age == 100) {
-        elegibleText.textContent = `Your age is ${age}. Here is your birthday card from the King!`;
+        elegibleText.textContent = `Here is your birthday card from the King!`;
     } else {
-        elegibleText.textContent = `Your age is ${age}. You are not elegible, you have already gotten one`;
+        elegibleText.textContent = `You are not elegible, you have already gotten one`;
     };
     ageInput.value = "";
 });
